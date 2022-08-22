769c002046cc1960fb2ac2451d5f95f5ba070f54
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-22 17:07:52 +0100

    changed Navbar linking and added ninja listing link to the homepage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
+import Link from 'next/Link';
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adip</p>
       <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );

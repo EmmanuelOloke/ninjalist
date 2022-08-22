122c5ce07388f9c843c37ea0d492a4bc5995cb44
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-22 18:17:05 +0100

    used Home.module.css styles

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,21 @@ import Link from 'next/Link';
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
         impedit suscipit architecto, odio inventore nostrum non neque dicta.
         Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
         nobis odit.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
         impedit suscipit architecto, odio inventore nostrum non neque dicta.
         Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
         nobis odit.
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );

0f8791eea77cc9c0985c4713ad47ac3e7eec632c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-22 23:56:51 +0100

    adding meta tag to the head

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
     <>
       <Head>
         <title> Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
       </Head>
       <div>
         <h1 className={styles.title}>Homepage</h1>

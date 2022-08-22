0d36f913d167008f12f25ea37a93f5c60d71e166
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-22 16:14:45 +0100

    added about.js page component

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+    </div>
+  );
+};
+
+export default About;

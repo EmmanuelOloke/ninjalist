de0264fd5dbfa4baa36e21095b1db108233ef72d
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-22 16:41:14 +0100

    created Navbar component and used it in Homepage(main) index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adip</p>
       <p>Lorem ipsum dolor sit amet, consectetur adip</p>
